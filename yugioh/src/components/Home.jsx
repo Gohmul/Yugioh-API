1e715a37655f6004ccde44008e93de54316bf32a
Ryan Saucier <ryanyoubum@gmail.com>
2022-11-07 11:06:49 -0500

    styling

--- yugioh/src/components/Home.jsx
+++ yugioh/src/components/Home.jsx
@@ -1,7 +1,15 @@
 export default function Home () {
     return (
         <div className="home">
-            <main>I am the home </main>
+            <main>
+                <div>
+                    <h1 className="what-is">What is this website?</h1>
+                    <p className="what-is-desc">This is a site that uses a Yu-gi-Oh Card API to pull information about Indivuals cards and values!</p>
+                    <h1 className="who-made">Who made this?</h1>
+                    <h1 className="who-made-desc">This website was made by Ryan Saucier</h1>
+
+                </div>
+            </main>
         </div>
     )
 }
